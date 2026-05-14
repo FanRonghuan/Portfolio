54541c75f2f74b073db9f36a48e22a27b6e4232a
FanRonghuan <rachel.ahua7@gmail.com>
2026-05-14 23:54:48 +0800

    chore: 配置 GitHub Actions 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // 这一行至关重要，必须与你的 GitHub 仓库名一致
 })
